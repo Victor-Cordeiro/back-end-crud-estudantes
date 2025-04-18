55dd2e73ea4e6e5554bdbac13d81388237387315
Victor-Cordeiro <victorcordeirodesousa@gmail.com>
2025-04-18 19:17:08 -0300

    Implementação do Controller

--- src/main/java/com/victorcordeiro/crud_estudantes/controller/EstudanteController.java
+++ src/main/java/com/victorcordeiro/crud_estudantes/controller/EstudanteController.java
@@ -4,9 +4,10 @@ package com.victorcordeiro.crud_estudantes.controller;
 import com.victorcordeiro.crud_estudantes.model.Estudante;
 import com.victorcordeiro.crud_estudantes.service.IEstudanteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +20,30 @@ public class EstudanteController {
 
     // Aqui você pode adicionar os métodos para lidar com as requisições HTTP
 
-    public ResponseEntity<List<Estudante>> getEstudantes() {
-        return
+    @GetMapping
+    public ResponseEntity<List<Estudante>> getAllEstudantes() {
+        return new ResponseEntity<>(estudanteService.getAllEstudantes(), HttpStatus.FOUND);
     }
+
+
+    @PostMapping
+    public Estudante addEstudante(@RequestBody Estudante estudante) {
+        return estudanteService.addEstudante(estudante);
+    }
+
+
+    @PutMapping("/update/{id}")
+    public Estudante updateEstudante(@RequestBody Estudante estudante, @PathVariable Long id) {
+        return estudanteService.updateEstudante(estudante, id);
+    }
+
+    public void deleteEstudante(@PathVariable Long id) {
+        estudanteService.deleteEstudante(id);
+    }
+
+
+
+
+
 
 }
